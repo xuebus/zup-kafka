f15bfb13e2054888ec9a278cea639d670890e9aa
Sandokan <sandokandias@gmail.com>
2016-09-29 18:41:57 -0300

    adding JavaTimeModule to jackson mapper

--- src/main/java/br/com/zup/kafka/consumer/deserializer/JsonDeserializer.java
+++ src/main/java/br/com/zup/kafka/consumer/deserializer/JsonDeserializer.java
@@ -3,16 +3,21 @@ package br.com.zup.kafka.consumer.deserializer;
 import br.com.zup.kafka.KafkaMessage;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.kafka.common.errors.SerializationException;
 import org.apache.kafka.common.serialization.Deserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.Map;
 
 public class JsonDeserializer implements Deserializer<Object> {
 
-    private static final ObjectMapper MAPPER = new ObjectMapper();
+    private static final Logger LOGGER = LoggerFactory.getLogger(JsonDeserializer.class);
+
+    private static final ObjectMapper MAPPER = new ObjectMapper().registerModule(new JavaTimeModule());
 
     private Class<?> clazz;
 
@@ -38,6 +43,7 @@ public class JsonDeserializer implements Deserializer<Object> {
         try {
             return MAPPER.readValue(data, type);
         } catch (IOException e) {
+            LOGGER.error("Error when deserializing byte[] to object", e);
             throw new SerializationException("Error when deserializing byte[] to object", e);
         }
     }
